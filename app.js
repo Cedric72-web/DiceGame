731e6cf41097d36d3b0a6be66eb86f13911c2e4b
VESQUE Cédric <vesque.cedric@bbox.fr>
2022-08-04 17:44:40 +0200

    Add rollDice & holdScore functions

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+// ----- * START DEFINITION DES VARIABLES * -----
+
 const newGame = document.getElementById('new-game');
 const roll = document.getElementById('roll');
 const hold = document.getElementById('hold');
 let player1Name = document.getElementById('player1-name');
 let player2Name = document.getElementById('player2-name');
+let player1TotalPoints = document.getElementById('player1-total-points');
+let player1CurrentPoints = document.getElementById('player1-current-points');
+let player2TotalPoints = document.getElementById('player2-total-points');
+let player2CurrentPoints = document.getElementById('player2-current-points');
 const rules = document.getElementById('rules');
+let roundScorePlayer1 = 0;
+let totalPlayer1 = 0;
+let holdScore = 0
+
+// ----- * END DEFINITION DES VARIABLES * -----
+
+// ----- * START FONCTION RULES * -----
 
 rules.addEventListener('click', () => {
   alert(`Règles du jeu :
@@ -15,6 +28,10 @@ rules.addEventListener('click', () => {
   Le premier joueur qui atteint les 100 points gagne le jeu.`)
 })
 
+// ----- * END FONCTION RULES * -----
+
+// ----- * START FONCTION NEWGAME * -----
+
 newGame.addEventListener('click', () => {
   player1 = prompt('Entrez le nom du joueur 1:');
   player1Name.innerText = player1;
@@ -22,12 +39,44 @@ newGame.addEventListener('click', () => {
   player2Name.innerText = player2;
   alert('Lancer la partie?');
   console.log('la partie commence...')
+  player2CurrentPoints = 0;
 });
+
+// ----- * END FONCTION NEWGAME * -----
+
+
+// ----- * START FONCTION ROLLDICE * -----
 
 roll.addEventListener('click', () => {
-  console.log('Lance le dés');
-});
+  let dice = document.getElementById('dice');
+
+  // Générer un nombre entre 1 et 6
+  let diceScore = Math.floor(Math.random() * 6 + 1);
+
+  const classDice = ['bi-dice-1', 'bi-dice-2', 'bi-dice-3', 'bi-dice-4', 'bi-dice-5', 'bi-dice-6']
+
+  dice.classList.remove(...classDice);
+  newDice = dice.classList.toggle('bi-dice-' + diceScore);
+
+  // Affichage du score du tour dans la case SCORE-TOUR
+  roundScorePlayer1 = roundScorePlayer1 + diceScore;
+  player1CurrentPoints.innerHTML = roundScorePlayer1;
+  
+  return roundScorePlayer1;
+})
+
+// ----- * END FONCTION ROLLDICE * -----
+
+// ----- * START FONCTION HOLDSCORE * -----
 
 hold.addEventListener('click', () => {
-  console.log('Conserve le score');
-});
+  // Affichage du score total du joueur1
+  holdScore = holdScore + roundScorePlayer1;
+  player1TotalPoints.innerHTML = holdScore;
+  
+  // Remise à zéro du score du tour
+  roundScorePlayer1 = 0;
+  document.getElementById('player1-current-points').innerHTML = roundScorePlayer1;
+})
+
+// ----- * END FONCTION HOLDSCORE * -----
