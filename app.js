d931da3a4b6ed9d8fd56f046a270d6cea0ca2e53
VESQUE Cédric <vesque.cedric@bbox.fr>
2022-08-04 09:54:45 +0200

    update style.css + app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Coucou!')
+const newGame = document.getElementById('new-game');
+const roll = document.getElementById('roll');
+const hold = document.getElementById('hold');
+
+newGame.addEventListener('click', () => {
+  console.log('Nouvelle partie');
+});
+
+roll.addEventListener('click', () => {
+  console.log('Lance le dés');
+});
+
+hold.addEventListener('click', () => {
+  console.log('Conserve le score');
+});
